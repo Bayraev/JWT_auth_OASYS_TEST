308449cfef28e5c4131b16817c0ef5bbe3bb9e0a
Alialisk <99403653+Aliasik@users.noreply.github.com>
2024-07-31 13:37:49 +0200

    Updated from garbage

--- Test_Oasys_JTW_backend/middlewares/getUsersMiddleware.js
+++ Test_Oasys_JTW_backend/middlewares/getUsersMiddleware.js
@@ -3,12 +3,10 @@ const { jwtService } = require('../services/tokenService');
 
 module.exports = async (req, res, next) => {
   try {
-    console.log('asas', req.userData);
+    // validate is userLvl okay to do this
     if (req.userData.type !== 1) {
       return next(res.json({ user: req.userData }));
     }
-    console.log(5);
-
     next();
   } catch (e) {
     return next(ApiError.UnauthorizedError());
