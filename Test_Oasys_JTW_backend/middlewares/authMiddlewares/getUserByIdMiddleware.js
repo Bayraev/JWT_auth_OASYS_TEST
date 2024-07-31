aa1c407d6286153cb21cc4d3a633a9187becf04d
Alialisk <99403653+Aliasik@users.noreply.github.com>
2024-07-31 15:24:52 +0200

    Fixed condition of access

--- Test_Oasys_JTW_backend/middlewares/authMiddlewares/getUserByIdMiddleware.js
+++ Test_Oasys_JTW_backend/middlewares/authMiddlewares/getUserByIdMiddleware.js
@@ -6,7 +6,11 @@ module.exports = async (req, res, next) => {
 
   console.log(user, id);
   // if user is not user who search his own id OR user is not 1-2+
-  if (user._id !== id || (user.type !== 1 && user.lvl < 2)) {
+  const condition1 = user.type > 1 && user.lvl < 2;
+  const condition2 = user._id !== id;
+  console.log('ADADADDA', condition1);
+
+  if (condition2 && condition1) {
     next(ApiError.PermissionDenied());
   }
 
